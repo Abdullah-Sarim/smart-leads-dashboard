a063c557450f97223c1202399b77f5eb4ca3d729
Sarim Abdullah <abdullahsarim1100@gmail.com>
2026-05-18 14:19:21 +0530

    feat: create lead model with status and source enums

--- server/src/types/index.ts
+++ server/src/types/index.ts
@@ -3,6 +3,19 @@ export enum UserRole {
   Sales = 'sales',
 }
 
+export enum LeadStatus {
+  New = 'New',
+  Contacted = 'Contacted',
+  Qualified = 'Qualified',
+  Lost = 'Lost',
+}
+
+export enum LeadSource {
+  Website = 'Website',
+  Instagram = 'Instagram',
+  Referral = 'Referral',
+}
+
 export type IUser = {
   _id: string;
   name: string;
@@ -14,3 +27,14 @@ export type IUser = {
 };
 
 export type IUserWithoutPassword = Omit<IUser, 'password'>;
+
+export type ILead = {
+  _id: string;
+  name: string;
+  email: string;
+  status: LeadStatus;
+  source: LeadSource;
+  createdBy: string;
+  createdAt: Date;
+  updatedAt: Date;
+};
